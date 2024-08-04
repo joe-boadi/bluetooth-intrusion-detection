274bf8103e7ac2ef130bc0148c1b544c2b5c0055
joe-boadi <joeboadi1@gmail.com>
2024-08-04 13:12:11 +0000

    Invoke block function on intrusion detection

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
+from realTime_detection import classify_connection
+from cli_interface import block_intrusion
+from model_training import model, scaler
+
 # Test scenarios
 test_samples = [
     {'connection_duration': 5, 'signal_strength': -60, 'data_transfer_rate': 3, 'time_of_day': 10},
     {'connection_duration': 20, 'signal_strength': -40, 'data_transfer_rate': 10, 'time_of_day': 22},
     {'connection_duration': 7, 'signal_strength': -55, 'data_transfer_rate': 2, 'time_of_day': 15},
-    {'connection_duration': 30, 'signal_strength': -30, 'data_transfer_rate': 15, 'time_of_day': 3}
+    {'connection_duration': 30, 'signal_strength': -30, 'data_transfer_rate': 15, 'time_of_day': 3},
+    {'connection_duration': 1, 'signal_strength': -90, 'data_transfer_rate': 50, 'time_of_day': 3}
 ]
 
 for sample in test_samples:
     prediction = classify_connection(model, scaler, sample)
-    status = "Intrusion Detected!" if prediction == 1 else "Normal Connection"
+    status = "Intrusion Detected!\n" if prediction == 1 else "Normal Connection"
     print(f"Data: {sample} => Status: {status}")
+    if prediction == 1:
+        block_intrusion(sample)
