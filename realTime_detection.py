541e2766876308af8ad1f56d027defca50c3577e
joe-boadi <joeboadi1@gmail.com>
2024-08-04 11:45:53 +0000

    Remove legacy functions of numpy by using the Generator alt

--- realTime_detection.py
+++ realTime_detection.py
@@ -1,13 +1,16 @@
 import time
-import numpy as np
+import numpy
+import pandas as pd
+from model_training import model, scaler
+
 
 def simulate_real_time_data(num_samples=10):
     for _ in range(num_samples):
         sample = {
-            'connection_duration': np.random.exponential(scale=10),
-            'signal_strength': np.random.normal(loc=-50, scale=10),
-            'data_transfer_rate': np.random.exponential(scale=5),
-            'time_of_day': np.random.uniform(low=0, high=24)
+            'connection_duration': numpy.random.Generator.exponential(scale=10),
+            'signal_strength': numpy.random.Generator.normal(loc=-50, scale=10),
+            'data_transfer_rate': numpy.random.Generator.exponential(scale=5),
+            'time_of_day': numpy.random.Generator.uniform(low=0, high=24)
         }
         yield sample
         time.sleep(1)  # Simulate real-time data stream
