f1d3e51072413dfd1097d812dd57b8e429ca8a1b
joe-boadi <joeboadi1@gmail.com>
2024-08-07 01:17:30 +0000

    Add more test samples

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ from model_training import model, scaler
 
 # Test scenarios
 test_samples = [
-    {'connection_duration': 5, 'signal_strength': -60, 'data_transfer_rate': 3, 'time_of_day': 10},
+    {'connection_duration': 7, 'signal_strength': -55, 'data_transfer_rate': 2, 'time_of_day': 15},
     {'connection_duration': 20, 'signal_strength': -40, 'data_transfer_rate': 10, 'time_of_day': 22},
     {'connection_duration': 7, 'signal_strength': -55, 'data_transfer_rate': 2, 'time_of_day': 15},
     {'connection_duration': 30, 'signal_strength': -30, 'data_transfer_rate': 15, 'time_of_day': 3},
-    {'connection_duration': 1, 'signal_strength': -90, 'data_transfer_rate': 50, 'time_of_day': 3}
+    {'connection_duration': 5, 'signal_strength': -60, 'data_transfer_rate': 3, 'time_of_day': 10}
 ]
 
 for sample in test_samples:
