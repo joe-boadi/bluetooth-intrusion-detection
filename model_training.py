0295624430f54c81bc32437b9485081fd74c05b1
joe-boadi <joeboadi1@gmail.com>
2024-08-14 10:12:00 +0000

    Modify model training py file

--- model_training.py
+++ model_training.py
@@ -1,22 +1,27 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report
 import pandas as pd
+from data_cleaning import preprocess_data
 
 
 # Load the dataset
 data = pd.read_csv('bluetooth_data.csv')
 
+X, y = preprocess_data('bluetooth_data1.csv')
+
 # Split the data into features and labels
 X = data.drop('label', axis=1)
 y = data['label']
 
+# Standardize the features
+scaler = StandardScaler()
+
 # Split into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Standardize the features
-scaler = StandardScaler()
+
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
@@ -31,5 +36,4 @@ precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 
 print(f'Accuracy: {accuracy:.2f}')
-print(f'Precision: {precision:.2f}')
-print(f'Recall: {recall:.2f}')
+# print(f'Report: {classification_report:.2f}')
